5dfc34223e32305f7cf5887d5c7822d77346e697
alejandro-nb <alejandro.navarro.bonilla@est.una.ac.cr>
2021-10-31 16:08:31 -0600

    Estructura aplicacion de escritorio

---
+++ cliente-escritorio/cliente/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.una.cliente {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.una.cliente to javafx.fxml;
+    exports org.una.cliente;
+}
